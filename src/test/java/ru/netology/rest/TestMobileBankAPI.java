2557eb9956d9fe8c244dc5f71bd0cea3b27be8d8
Yuliya Tonkikh <uliya_i@bk.ru>
2023-03-15 16:09:47 +0700

    customize tests API. add schema validation test

--- src/test/java/ru/netology/rest/TestMobileBankAPI.java
+++ src/test/java/ru/netology/rest/TestMobileBankAPI.java
@@ -1,8 +1,10 @@
 package ru.netology.rest;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 
 class TestMobileBankAPI {
@@ -11,10 +13,20 @@ class TestMobileBankAPI {
     */
 
     @Test
+    @DisplayName("Should 200Ok on get /demo/accounts")
     void shouldReturnDemoAccounts() {
         given().baseUri("http://localhost:9999/api/v1").
                 when().get("/demo/accounts").
                 then().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Should return demo accounts with equal schema")
+    void shouldReturnDemoAccountsWithEqSchema() {
+        given().baseUri("http://localhost:9999/api/v1").
+                when().get("/demo/accounts").
+                then().statusCode(200).
+                body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 
 
