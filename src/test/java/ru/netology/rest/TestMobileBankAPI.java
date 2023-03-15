2087e7d4dbda549776666035fb1d96b841777eb7
Yuliya Tonkikh <uliya_i@bk.ru>
2023-03-15 17:40:16 +0700

    done :rocket:

--- src/test/java/ru/netology/rest/TestMobileBankAPI.java
+++ src/test/java/ru/netology/rest/TestMobileBankAPI.java
@@ -31,45 +31,4 @@ class TestMobileBankAPI {
                 then().statusCode(200).
                 body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
-
-    @Test
-    @DisplayName("should currency RUB")
-    void shouldCurrencyRub() {
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .body("[0].currency", equalTo("RUB"));
-
-    }
-
-    @Test
-    @DisplayName("Should currency USD")
-    void shouldCurrencyUsd() {
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .body("[1].currency", equalTo("USD"));
-
-    }
-
-    @Test
-    @DisplayName("Should currency RUB and USD")
-    void shouldCurrencyRubUsd() {
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .contentType(ContentType.JSON)
-                .body("", hasSize(3))
-                .body("[2].currency", equalTo("RUB"))
-                .body("[1].currency", equalTo("USD"))
-                .body("[0].balance", greaterThanOrEqualTo(0));
-
-    }
-
 }
