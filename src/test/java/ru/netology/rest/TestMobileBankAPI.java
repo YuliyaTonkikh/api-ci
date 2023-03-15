cbfd8ebbb2341eaec519afcf55b4d3c782e6bea4
Yuliya Tonkikh <uliya_i@bk.ru>
2023-03-15 17:43:20 +0700

    fix

--- src/test/java/ru/netology/rest/TestMobileBankAPI.java
+++ src/test/java/ru/netology/rest/TestMobileBankAPI.java
@@ -5,9 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.*;
-
-import io.restassured.http.ContentType;
 
 
 class TestMobileBankAPI {
